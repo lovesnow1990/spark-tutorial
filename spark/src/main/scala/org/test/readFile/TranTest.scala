e936a803dc578cb9e763d23f280b0af434ddca5a
Anthony <lovesnow1990@gmail.com>
2016-01-11 18:18:33 +0800

    update

--- spark/src/main/scala/org/test/readFile/TranTest.scala
+++ spark/src/main/scala/org/test/readFile/TranTest.scala
@@ -14,10 +14,9 @@ object TranTest {
     val disdata = sc.parallelize(data)
     //disdata.foreach(println)
     
-    //val rdd = sc.parallelize(Seq(Seq(1, 2, 3), Seq(4, 5, 6), Seq(7, 8, 9)))
+    val two = disdata.map(line => line.split(","))
+    val rdd = sc.parallelize(Seq(Seq(1, 2, 3), Seq(4, 5, 6), Seq(7, 8, 9)))
     //rdd.foreach(println)
-    
-    
     // Split the matrix into one number per line.
     val byColumnAndRow = disdata.zipWithIndex.flatMap {
       case (row, rowIndex) => row.zipWithIndex.map {
@@ -30,7 +29,6 @@ object TranTest {
     val transposed = byColumn.map {
       indexedRow => indexedRow.toSeq.sortBy(_._1).map(_._2)
     }
-    println(transposed)
     println("finish")
   }
 }
